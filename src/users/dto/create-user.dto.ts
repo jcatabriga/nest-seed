5ff97a9b12f01c64e2ca8d626c3466e4bea8e7aa
Gabriel Silva <devzgabriel@gmail.com>
2021-12-22 17:34:55 -0300

    chore: configure Swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,34 @@
-import { IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  /**
+   * The user name that will be used to show the profile.
+   * @example 'John Doe'
+   */
   @IsString()
+  @IsNotEmpty()
   name: string;
 
-  @IsString()
+  /**
+   * The user email that will be used to login.
+   * @example 'example@email.com'
+   */
+  @IsEmail()
   email: string;
 
+  /**
+   * The user password that will be used to login.
+   * @example '1a2b3c4d5e6'
+   */
   @IsString()
+  @IsNotEmpty()
+  @MaxLength(20)
+  @MinLength(8)
   password: string;
 }
