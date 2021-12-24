94450ffc03fa7adbe47a535b27b1418ccd221eaf
Gabriel Silva <devzgabriel@gmail.com>
2021-12-24 12:23:21 -0300

    chore: create seed and new tables

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,47 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+async function createUsersAndRoles() {
+  const ADMIN_ID = '1';
+
+  await prisma.user.createMany({
+    data: [
+      {
+        id: ADMIN_ID,
+        email: 'admin@eurekalabs.com.br',
+        password: 'admin',
+        name: 'admin',
+      },
+      {
+        id: '2',
+        email: 'shoulders@eurekalabs.com.br',
+        password: 'shoulders',
+        name: 'shoulders',
+      },
+      {
+        id: '3',
+        email: 'user@eurekalabs.com.br',
+        password: 'user',
+        name: 'user',
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  await prisma.role.create({
+    data: {
+      name: 'admin',
+      User: {
+        connect: {
+          id: ADMIN_ID,
+        },
+      },
+    },
+  });
+}
+
 async function main() {
-  console.log('Seeding...');
+  await createUsersAndRoles();
 }
 
 main()
